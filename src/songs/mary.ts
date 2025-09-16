04688d64c6e3e5cfea9bfc24e30526c034a997c4
Leonardo Merza <ljmerza@gmail.com>
2025-09-15 22:30:19 -0400

    y

--- src/songs/mary.ts
+++ src/songs/mary.ts
@@ -1,24 +1,67 @@
-import type { KidsSong } from '../types';
+import type { KidsSong } from "../types";
 
 export const mary: KidsSong = {
-  title: 'Mary Had a Little Lamb',
+  title: "Mary Had a Little Lamb",
   tempo: 100,
   time: { num: 4, den: 4 },
   measures: [
     {
       notes: [
-        { fret: 0, string: 3, dur: 'q' },
-        { fret: 2, string: 1, dur: 'q' },
-        { fret: 4, string: 1, dur: 'q' },
-        { fret: 2, string: 1, dur: 'q' },
+        { fret: 4, string: 1, dur: "q" },
+        { fret: 2, string: 1, dur: "q" },
+        { fret: 0, string: 1, dur: "q" },
+        { fret: 2, string: 1, dur: "q" },
       ],
     },
     {
       notes: [
-        { fret: 0, string: 1, dur: 'q' },
-        { fret: 0, string: 1, dur: 'q' },
-        { fret: 0, string: 1, dur: 'h' },
+        { fret: 4, string: 1, dur: "q" },
+        { fret: 4, string: 1, dur: "q" },
+        { fret: 4, string: 1, dur: "h" },
+      ],
+    },
+    {
+      notes: [
+        { fret: 2, string: 1, dur: "q" },
+        { fret: 2, string: 1, dur: "q" },
+        { fret: 2, string: 1, dur: "h" },
+      ],
+    },
+    {
+      notes: [
+        { fret: 4, string: 1, dur: "q" },
+        { fret: 7, string: 1, dur: "q" },
+        { fret: 7, string: 1, dur: "h" },
+      ],
+    },
+    {
+      notes: [
+        { fret: 4, string: 1, dur: "q" },
+        { fret: 2, string: 1, dur: "q" },
+        { fret: 0, string: 1, dur: "q" },
+        { fret: 2, string: 1, dur: "q" },
+      ],
+    },
+    {
+      notes: [
+        { fret: 4, string: 1, dur: "q" },
+        { fret: 4, string: 1, dur: "q" },
+        { fret: 4, string: 1, dur: "q" },
+        { fret: 4, string: 1, dur: "q" },
+      ],
+    },
+    {
+      notes: [
+        { fret: 2, string: 1, dur: "q" },
+        { fret: 2, string: 1, dur: "q" },
+        { fret: 4, string: 1, dur: "q" },
+        { fret: 2, string: 1, dur: "q" },
+      ],
+    },
+    {
+      notes: [
+        { fret: 0, string: 1, dur: "w" },
       ],
     },
   ],
-}
+};
